8dbf6bb95de3bdd9aa292f1d2fc3e3bc5407e1b4
Martin <martin-1998@hotmail.com>
2019-09-25 12:52:38 -0500

    Added living room light commands

---
+++ src/command/LivingLightOnCommand.java
@@ -0,0 +1,14 @@
+package command;
+
+public class LivingLightOnCommand implements Command {
+    LivingLight l_light;
+
+    public LivingLightOnCommand(LivingLight l_light) {
+        this.l_light = l_light;
+    }
+
+    @Override
+    public void execute() {
+        l_light.on();
+    }
+}
