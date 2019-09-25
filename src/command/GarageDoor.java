553b3827c0b6704000cdc5db665236e904d5cd4e
Andrea Diaz <andrea.diaz.gardini@gmail.com>
2019-09-25 12:22:25 -0500

    Garage Door

---
+++ src/command/GarageDoor.java
@@ -0,0 +1,15 @@
+package command;
+
+public class GarageDoor {
+    private boolean isOpen = false;
+    public void  open(){
+        isOpen = true;
+        System.out.println("Door is "+isOpen);
+    }
+
+    public void close(){
+        isOpen = false;
+        System.out.println("Door is "+isOpen);
+    }
+
+}
